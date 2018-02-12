f3f420f5907b22842b9a59e4c52c5b2afdfcc673
Ryan Cheley <rcheley@gmail.com>
2018-02-11 20:52:37 -0800

    Added ability to parse the csv schedule of the Dodgers for the 2018 season

--- program.py
+++ program.py
@@ -1,21 +1,27 @@
-#from sense_hat import SenseHat
+from sense_hat import SenseHat
 from datetime import datetime
 import pytz
 import os
 import csv
-
+from dateutil.relativedelta import relativedelta
 from data_types import Schedule
 
 
 def main():
     filename = get_data_file()
     data = load_file(filename)
-    #sense = SenseHat()
+    sense = SenseHat()
     utc_now = pytz.utc.localize(datetime.utcnow())
-    now = pst_now = utc_now.astimezone(pytz.timezone("America/Los_Angeles"))
-    message = '#ITFDB!!!'
-    print(now)
-    print(data)
+    now = utc_now.astimezone(pytz.timezone("America/Los_Angeles"))
+    for game in data:
+        game_date_time = datetime.strptime(game.game_date_time, '%Y-%m-%d %I:%M %p')
+        game_date_time = game_date_time.astimezone(pytz.timezone("America/Los_Angeles"))
+        minute_diff = relativedelta(game_date_time, now).minutes
+        hour_diff = relativedelta(game_date_time, now).hours
+        day_diff = relativedelta(game_date_time, now).days
+        if day_diff == 0 and hour_diff == 0 and 10 >= minute_diff >= 0:
+            message = '#ITFDB!!!'
+            print(message)
 
 
 def get_data_file():
